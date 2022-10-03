822af994138d76a1bc384ad0ae546f7b02caaa65
ccfries <cfries@google.com>
2022-10-03 12:46:17 +0000

    ginkgo: Set VENDOR_SECURITY_PATCH to PLATFORM
    Set the VENDOR_SECURITY_PATCH level to match the platform security patch level.
    These are functionally the same.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # Security patch level - V12.5.12.0.RCOEUXM
 BOOT_SECURITY_PATCH := 2022-10-01
-VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Inherit from the proprietary version
 include vendor/xiaomi/ginkgo/BoardConfigVendor.mk
