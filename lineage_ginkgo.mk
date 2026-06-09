0e658bd70f63188519d558fc81ae840dc0976db2
4admn <muhammedyrlk361@gmail.com>
2026-06-09 17:29:39 +0300

    Update lineage_ginkgo.mk

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
 # Enable UI enhancements
-TARGET_ENABLE_BLUR := false
+TARGET_ENABLE_BLUR := true
 PERF_ANIM_OVERRIDE := true
 
 # Enable features
@@ -23,6 +23,13 @@ TARGET_SUPPORTS_QUICK_TAP := true
 BYPASS_CHARGE_SUPPORTED := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 USE_PIXEL_CHARGING := true
+
+# Dolby
+$(call inherit-product-if-exists, hardware/dolby/dolby.mk)
+
+# MiCam Port
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-ginkgo/device.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera-ginkgo/miuicamera-ginkgo-vendor.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_ginkgo
