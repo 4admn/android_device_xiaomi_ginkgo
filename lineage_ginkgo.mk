3b045ef735a00c3f6441830784c8f860ddc5698b
4admn <muhammedyrlk361@gmail.com>
2026-08-24 03:02:14 +0300

    Dynamic ....

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -24,6 +24,9 @@ BYPASS_CHARGE_SUPPORTED := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 USE_PIXEL_CHARGING := true
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Dolby
 $(call inherit-product-if-exists, hardware/dolby/dolby.mk)
 
